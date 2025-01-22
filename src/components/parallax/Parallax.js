b6dc9db3b1bea4fad88b765b9a6cac72c60dbe3c
unknown <sourabkapoor@yahoo.com>
2025-01-22 15:50:09 -0500

    Decreased parallax motion speed

--- src/components/parallax/Parallax.js
+++ src/components/parallax/Parallax.js
@@ -28,7 +28,7 @@ export const Parallax = ({children, baseVelocity = 100}) => {
 
   const directionFactor = useRef(1);
   useAnimationFrame((t, delta) => {
-    let moveBy = directionFactor.current * baseVelocity * (delta / 3000);
+    let moveBy = directionFactor.current * baseVelocity * (delta / 6000);
 
     if (velocityFactor.get() < 0) {
       directionFactor.current = -1;
@@ -44,6 +44,8 @@ export const Parallax = ({children, baseVelocity = 100}) => {
   return (
     <div className="parallax">
       <motion.div className="scroller" style={{ x }}>
+        <span>{children} </span>
+        <span>{children} </span>
         <span>{children} </span>
         <span>{children} </span>
         <span>{children} </span>
